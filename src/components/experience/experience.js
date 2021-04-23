4e762e4a7fa4f3749148903f8e91b5494c82b17c
Ademir Braga <ademirbraga@softbox.com.br>
2021-04-23 20:50:56 -0300

    refaturação

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -8,11 +8,11 @@ const Experience = (props) => (
   <Card>
       <Row>
         <Col s={2} m={2}>
-          <img src={props.avatar} className="circle responsive-img" />
+          <img src={props.avatar} className="circle responsive-img" alt=""/>
           { props.name }
         </Col>
 
-        <Col s={10} s={10}>
+        <Col s={10} >
           <p><b>{props.title} <span className="grey darken-2 white-text">{props.company}</span></b></p>
           <p>{props.description}</p>
         </Col>
